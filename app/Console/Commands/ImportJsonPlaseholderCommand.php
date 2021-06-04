0561ee7ddabf546688cb7d20818d9fdc4bcd8eba
Your Name <you@example.com>
2021-06-04 13:29:49 +0300

    i get my notes

--- app/Console/Commands/ImportJsonPlaseholderCommand.php
+++ app/Console/Commands/ImportJsonPlaseholderCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Components\ImportDataClient;
+use App\Models\Folder;
+use App\Models\Note;
 use Illuminate\Console\Command;
 
 class ImportJsonPlaseholderCommand extends Command
@@ -45,17 +47,23 @@ class ImportJsonPlaseholderCommand extends Command
             ]
         ]);
         $data = (json_decode($responce->getBody()->getContents()));
-        foreach ($data as $item) {
-            $folder = substr($item->folderPathname, 1);
-            Folder::firstOrCreate(
-                ['title' => $folder],
-                ['title' => $folder]);
-            $key = Folder::where('title', $folder)->get();
-            Note::firstOrCreate(
-                ['title' => $item->title],
-                [   'title' => $item->title,
-                    'content' => $item->head->content,
-                    'folder_id' => $key->id]);
+
+        foreach ($data as $arr) {
+            foreach ($arr as $item){
+                $str = $item->folderPathname;
+                $folder = substr($str, 1);
+                Folder::firstOrCreate(
+                    ['title' => $folder],
+                    ['title' => $folder]);
+                $key = Folder::where('title', $folder)->get();
+
+                Note::firstOrCreate(
+                    ['title' => $item->title],
+                    [   'title' => $item->title,
+                        'content' => $item->head->content,
+                        'folder_id' => $key[0]->id
+                    ]);
+            }
         }
     }
 }
--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+    protected $guarded = false;
 
     public function folders()
     {
